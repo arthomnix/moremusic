f5a5cf33720b0d98eec6aa22bba8fb6e2aeb20f3
arthomnix <arthomnix@arthomnix.dev>
2022-05-28 09:15:44 +0100

    Config tooltips

--- src/main/java/dev/arthomnix/moremusic/config/MoreMusicConfig.java
+++ src/main/java/dev/arthomnix/moremusic/config/MoreMusicConfig.java
@@ -8,19 +8,22 @@ import me.shedaniel.autoconfig.annotation.ConfigEntry;
 @Config.Gui.Background("minecraft:textures/block/note_block.png")
 public class MoreMusicConfig implements ConfigData {
     public static class MusicDelays {
+        @ConfigEntry.Gui.Tooltip
         public int minDelay = 20;
+
+        @ConfigEntry.Gui.Tooltip
         public int maxDelay = 40;
     }
 
-    @ConfigEntry.Gui.CollapsibleObject
+    @ConfigEntry.Gui.CollapsibleObject(startExpanded = true)
     public MusicDelays menuDelays = new MusicDelays();
 
-    @ConfigEntry.Gui.CollapsibleObject
+    @ConfigEntry.Gui.CollapsibleObject(startExpanded = true)
     public MusicDelays normalGameplayDelays = new MusicDelays();
 
-    @ConfigEntry.Gui.CollapsibleObject
+    @ConfigEntry.Gui.CollapsibleObject(startExpanded = true)
     public MusicDelays creativeGameplayDelays = new MusicDelays();
 
-    @ConfigEntry.Gui.CollapsibleObject
+    @ConfigEntry.Gui.CollapsibleObject(startExpanded = true)
     public MusicDelays endGameplayDelays = new MusicDelays();
 }
