7a6be049f54e1d5852c78356090deda21a98f260
arthomnix <arthomnix@arthomnix.dev>
2023-08-21 17:30:59 +0100

    Update to 1.20.1

--- src/main/java/dev/arthomnix/moremusic/mixin/MusicTypeMixin.java
+++ src/main/java/dev/arthomnix/moremusic/mixin/MusicTypeMixin.java
@@ -4,6 +4,7 @@ import dev.arthomnix.moremusic.MoreMusicPreLaunchEntryPoint;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.client.sound.MusicType;
+import net.minecraft.registry.entry.RegistryEntry;
 import net.minecraft.sound.MusicSound;
 import net.minecraft.sound.SoundEvent;
 import org.spongepowered.asm.mixin.Mixin;
@@ -11,12 +12,12 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
 // so it turns out mcdev really doesn't like new object redirects
-@SuppressWarnings({"InvalidMemberReference", "InvalidInjectorMethodSignature", "MixinAnnotationTarget", "UnresolvedMixinReference"})
+@SuppressWarnings({"InvalidInjectorMethodSignature", "MixinAnnotationTarget", "UnresolvedMixinReference"})
 @Environment(EnvType.CLIENT)
 @Mixin(MusicType.class)
 public class MusicTypeMixin {
     @Redirect(method = "<clinit>", at = @At(value = "NEW", target = "Lnet/minecraft/sound/MusicSound;", ordinal = 0))
-    private static MusicSound modifyMenuDelays(SoundEvent sound, int minDelay, int maxDelay, boolean replaceCurrentMusic) {
+    private static MusicSound modifyMenuDelays(RegistryEntry<SoundEvent> sound, int minDelay, int maxDelay, boolean replaceCurrentMusic) {
         return new MusicSound(
                 sound,
                 MoreMusicPreLaunchEntryPoint.config.menuDelays.minDelay,
@@ -26,7 +27,7 @@ public class MusicTypeMixin {
     }
 
     @Redirect(method = "<clinit>", at = @At(value = "NEW", target = "Lnet/minecraft/sound/MusicSound;", ordinal = 1))
-    private static MusicSound modifyCreativeDelays(SoundEvent sound, int minDelay, int maxDelay, boolean replaceCurrentMusic) {
+    private static MusicSound modifyCreativeDelays(RegistryEntry<SoundEvent> sound, int minDelay, int maxDelay, boolean replaceCurrentMusic) {
         return new MusicSound(
                 sound,
                 MoreMusicPreLaunchEntryPoint.config.creativeGameplayDelays.minDelay,
@@ -36,7 +37,7 @@ public class MusicTypeMixin {
     }
 
     @Redirect(method = "<clinit>", at = @At(value = "NEW", target = "Lnet/minecraft/sound/MusicSound;", ordinal = 4))
-    private static MusicSound modifyEndDelays(SoundEvent sound, int minDelay, int maxDelay, boolean replaceCurrentMusic) {
+    private static MusicSound modifyEndDelays(RegistryEntry<SoundEvent> sound, int minDelay, int maxDelay, boolean replaceCurrentMusic) {
         return new MusicSound(
                 sound,
                 MoreMusicPreLaunchEntryPoint.config.endGameplayDelays.minDelay,
@@ -46,7 +47,7 @@ public class MusicTypeMixin {
     }
 
     @Redirect(method = "createIngameMusic", at = @At(value = "NEW", target = "Lnet/minecraft/sound/MusicSound;"))
-    private static MusicSound modifyIngameMusicDelays(SoundEvent sound, int minDelay, int maxDelay, boolean replaceCurrentMusic) {
+    private static MusicSound modifyIngameMusicDelays(RegistryEntry<SoundEvent> sound, int minDelay, int maxDelay, boolean replaceCurrentMusic) {
         return new MusicSound(
                 sound,
                 MoreMusicPreLaunchEntryPoint.config.normalGameplayDelays.minDelay,
